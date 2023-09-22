146a150488e093eca1a4d16fcec2db340a296e7c
Rahem <sorourrahem17@gmail.com>
2023-09-22 06:08:05 +0100

    completed assignement

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -8,19 +8,18 @@ function ColorList() {
     "darkslategray",
     "hotpink",
   ];
+  const colorelemt=colors.map((color)=>{
+   return <h1 key={color} style={{color:color}}>{color}</h1>})
+
 
   return (
-    <div>
-      <h1>Top 5 CSS Colors</h1>
-      <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
-      </ol>
+   <div>
+     <h1>Top 5 CSS Colors</h1>
+     <ol>{colorelemt}</ol>
+     
     </div>
-  );
-}
+  
+  )}
+
 
 export default ColorList;
